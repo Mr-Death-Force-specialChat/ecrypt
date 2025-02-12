4236000db01e4a43c42cb1a5f49099ea5b7f911d
Mr-Death-Force-specialChat <84960714+Mr-Death-Force-specialChat@users.noreply.github.com>
2025-02-13 00:06:27 +0300

    Added encode_all to abstract get_size functions for Base64 and Hex.

--- ecrypt_encode_hex.h
+++ ecrypt_encode_hex.h
@@ -20,6 +20,12 @@ namespace ecrypt { namespace encode { namespace hex
 
 			encoder.Get(result, result_len);
 		}
+		void nrencode_all(const std::string& data, std::string* result)
+		{
+			size_t result_len = encode_get_size(reinterpret_cast<const unsigned char*>(data.data()), data.size());
+			result->resize(result_len);
+			nrencode(reinterpret_cast<const unsigned char*>(data.data()), data.size(), reinterpret_cast<unsigned char*>(result->data()), result->size());
+		}
 
 		size_t encode_get_size(const unsigned char* data, const size_t& data_len)
 		{
@@ -35,6 +41,13 @@ namespace ecrypt { namespace encode { namespace hex
 			encoder.MessageEnd();
 
 			encoder.Get(result, result_len);
+			return result;
+		}
+		std::string* encode_all(const std::string& data, std::string* result)
+		{
+			size_t result_len = encode_get_size(reinterpret_cast<const unsigned char*>(data.data()), data.size());
+			result->resize(result_len);
+			nrencode(reinterpret_cast<const unsigned char*>(data.data()), data.size(), reinterpret_cast<unsigned char*>(result->data()), result->size());
 			return result;
 		}
 	};
@@ -56,6 +69,12 @@ namespace ecrypt { namespace encode { namespace hex
 
 			encoder.Get(result, result_len);
 		}
+		inline void nrencode_all(const std::string& data, std::string* result)
+		{
+			size_t result_len = encode_get_size(reinterpret_cast<const unsigned char*>(data.data()), data.size());
+			result->resize(result_len);
+			nrencode(reinterpret_cast<const unsigned char*>(data.data()), data.size(), reinterpret_cast<unsigned char*>(result->data()), result->size());
+		}
 
 		inline size_t encode_get_size(const unsigned char* data, const size_t& data_len)
 		{
@@ -71,6 +90,13 @@ namespace ecrypt { namespace encode { namespace hex
 			encoder.MessageEnd();
 
 			encoder.Get(result, result_len);
+			return result;
+		}
+		inline std::string* encode_all(const std::string& data, std::string* result)
+		{
+			size_t result_len = encode_get_size(reinterpret_cast<const unsigned char*>(data.data()), data.size());
+			result->resize(result_len);
+			nrencode(reinterpret_cast<const unsigned char*>(data.data()), data.size(), reinterpret_cast<unsigned char*>(result->data()), result->size());
 			return result;
 		}
 	};
@@ -93,6 +119,12 @@ namespace ecrypt { namespace encode { namespace hex
 
 			decoder.Get(result, result_len);
 		}
+		void nrdecode_all(const std::string& data, std::string* result)
+		{
+			size_t result_len = decode_get_size(reinterpret_cast<const unsigned char*>(data.data()), data.size());
+			result->resize(result_len);
+			nrdecode(reinterpret_cast<const unsigned char*>(data.data()), data.size(), reinterpret_cast<unsigned char*>(result->data()), result->size());
+		}
 
 		size_t decode_get_size(const unsigned char* data, const size_t& data_len)
 		{
@@ -108,6 +140,13 @@ namespace ecrypt { namespace encode { namespace hex
 			decoder.MessageEnd();
 
 			decoder.Get(result, result_len);
+			return result;
+		}
+		std::string* decode_all(const std::string& data, std::string* result)
+		{
+			size_t result_len = decode_get_size(reinterpret_cast<const unsigned char*>(data.data()), data.size());
+			result->resize(result_len);
+			nrdecode(reinterpret_cast<const unsigned char*>(data.data()), data.size(), reinterpret_cast<unsigned char*>(result->data()), result->size());
 			return result;
 		}
 	};
@@ -129,6 +168,12 @@ namespace ecrypt { namespace encode { namespace hex
 
 			decoder.Get(result, result_len);
 		}
+		inline void nrdecode_all(const std::string& data, std::string* result)
+		{
+			size_t result_len = decode_get_size(reinterpret_cast<const unsigned char*>(data.data()), data.size());
+			result->resize(result_len);
+			nrdecode(reinterpret_cast<const unsigned char*>(data.data()), data.size(), reinterpret_cast<unsigned char*>(result->data()), result->size());
+		}
 
 		inline size_t decode_get_size(const unsigned char* data, const size_t& data_len)
 		{
@@ -144,6 +189,13 @@ namespace ecrypt { namespace encode { namespace hex
 			decoder.MessageEnd();
 
 			decoder.Get(result, result_len);
+			return result;
+		}
+		inline std::string* decode_all(const std::string& data, std::string* result)
+		{
+			size_t result_len = decode_get_size(reinterpret_cast<const unsigned char*>(data.data()), data.size());
+			result->resize(result_len);
+			nrdecode(reinterpret_cast<const unsigned char*>(data.data()), data.size(), reinterpret_cast<unsigned char*>(result->data()), result->size());
 			return result;
 		}
 	};
